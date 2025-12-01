413d346b72e30904bf3fb51c13493ef7c24d81b9
khcmh2509 <huni1905@gmail.com>
2025-12-01 17:47:23 +0900

    no message

--- todoList/src/main/java/edu/kh/todo/common/config/DBConfig.java
+++ todoList/src/main/java/edu/kh/todo/common/config/DBConfig.java
@@ -16,13 +16,13 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-/* @Configuration 
+/* @Configuration
  * - 스프링 설정용 클래스임 명시 
  * (스프링이 해당 클래스 설정 정보로 인식하고 사용)
  * + 객체로 생성해서 내부 코드를 서버 실행 시 모두 바로 실행
  * 
  * @PropertySource : properties 파일의 내용을 이용하겠다는 어노테이션
- * 다른 properties도 추가하고 싶으면 어노테이션을 계속 추가 
+ * 다른 properties도 추가하고 싶으면 어노테이션을 계속 추가
  * */
 
 @Configuration
@@ -34,10 +34,10 @@ public class DBConfig {
 	private ApplicationContext applicationContext;
 	// application scope 객체 : 즉, 현재 프로젝트 자체
 	// -> 현재 프로젝트의 전반적인 DB 설정과, Bean 관리에 접근할 수 있도록 해줌
-	// -> 스프링이 관리하고 있는 ApplicationContext 객체를 의존성 주입받는다.
+	// -> 스프링이 관리하고있는 ApplicationContext 객체를 의존성 주입받는다.
 
 	// 메서드
-	///////////////////////////// HikariCP 설정 //////////////////////////
+	/////////////////////////// HikariCP 설정 //////////////////////////////
 
 	// @Bean
 	// - 개발자가 수동으로 bean을 등록하는 어노테이션
@@ -66,7 +66,7 @@ public class DBConfig {
 		DataSource dataSource = new HikariDataSource(config);
 
 		// DataSource :
-		// 애플리케이션이 데이터베이스에 연결할 때 사용하는 설정 객체
+		// 애플리케이션이 데이터베이스에 연결할 때 사용하는 설정.
 		// 1) DB 연결 정보 제공(url, username, password)
 		// 2) Connection pool 관리 (Connection 생성/생명주기 관리)
 		// 3) 트랜잭션 관리
@@ -74,32 +74,36 @@ public class DBConfig {
 	}
 
 	////////////////////////////Mybatis 설정 추가 ////////////////////////////
-	// Mybatis : Java 애플리케이션에서 SQL을 더 쉽게할 수 있도록 도와주는 
+	// Mybatis : Java 애플리케이션에서 SQL을 더 쉽게 사용할 수 있도록 도와주는
 	// 			영속성 프레임워크
-	// 영속성  프레임워크(Persistence Framework)는 애플리케이션의 데이터를
-	// 데이터베이스와 같은 저장소에 영구적으로 저장하고,
+	// 영속성 프레임워크(Persistence Framework)는 애플리케이션의 데이터를
+	// 데이터베이스와 같은 저장소에 영구적으로 저장하고, 
 	// 이를 쉽게 CRUD 할 수 있도록 도와주는 프레임워크
-
-	//SqlSessionFactory : SqlSession을 만드는 객체	
+	
+	//SqlSessionFactory : SqlSession을 만드는 객체
 	@Bean
-	public SqlSessionFactory sessionFactory(DataSource dataSource) throws Exception {
-
+	public SqlSessionFactory sessionFactory(DataSource dataSource) throws Exception{
+		
 		SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
 		sessionFactoryBean.setDataSource(dataSource);
-
-		// 세팅 1. 매퍼 파일이 모여있는 경로 지정
-		sessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**.xml"));
-
+		
+		//세팅 1. 매퍼 파일이 모여있는 경로 지정
+		sessionFactoryBean.setMapperLocations(
+				applicationContext.getResources("classpath:/mappers/**.xml"));
+		
 		// 세팅 2. 별칭을 지정해야하는 DTO가 모여있는 패키지 지정
 		// -> 해당 패키지에 있는 모든 클래스가 클래스명으로 별칭이 지정됨
 		sessionFactoryBean.setTypeAliasesPackage("edu.kh.todo");
-		// -> edu.kh.todo 세팅 시 패키지 하위에 있는 모든 클래스가
+		// -> edu.kh.todo 세팅 시 패키지 하위에 있는 모든 클래스가 
 		// 클래스명으로 별칭이 지정됨.
 		// -> ex) edu.kh.todo.model.dto.Todo -> Todo (별칭 등록)
-
+		
+		
 		// 세팅 3. 마이바티스 설정 파일 경로 지정
-		sessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
-
+		sessionFactoryBean.setConfigLocation(
+				applicationContext.getResource("classpath:mybatis-config.xml"));
+		
+		
 		// SqlSession 객체 반환
 		return sessionFactoryBean.getObject();
 	}
---
+++ todoList/src/main/java/edu/kh/todo/model/dto/Todo.java
@@ -0,0 +1,21 @@
+package edu.kh.todo.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Getter + Setter + toString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+	private int todoNo; // 할 일 번호 (DB에는 TODO_NO)
+	private String todoTitle; // 할 일 제목(TODO_TITLE)
+	private String todoContent; // 할 일 제목(TODO_CONTENT)
+	private String Complete; // 할 일 완료여부(COMPLETE "Y"/"N")
+	private String regDate; // 할 일 등록일(REG_DATE)
+	
+	
+	
+}
