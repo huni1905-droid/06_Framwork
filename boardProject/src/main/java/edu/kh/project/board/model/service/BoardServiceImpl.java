a4a3d24ac6f8f6add4b590a4d48e2cf3444bea9b
huni1905-droid <huni1905@gmail.com>
2025-12-12 17:50:28 +0900

    no message

---
+++ boardProject/src/main/java/edu/kh/project/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,27 @@
+package edu.kh.project.board.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.project.board.model.mapper.BoardMapper;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+@Slf4j
+public class BoardServiceImpl implements BoardService{
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	// 게시판 종류 조회 서비스
+	@Override
+	public List<Map<String, Object>> selectBoardTypeList() {
+		return mapper.selectBoardTypeList();
+	}
+	
+}
