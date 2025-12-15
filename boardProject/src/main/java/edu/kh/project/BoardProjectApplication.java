621b1839bee6ac3f3c4ea9b9611123bc361dcefc
huni1905-droid <huni1905@gmail.com>
2025-12-15 17:53:02 +0900

    no message

--- boardProject/src/main/java/edu/kh/project/BoardProjectApplication.java
+++ boardProject/src/main/java/edu/kh/project/BoardProjectApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) 
-// @SpringBootApplication스프링이 굴러가기위한 필수 설정
-// (exclude = {SecurityAutoConfiguration.class}) : Spring Security에서 기본제공하는 로그인 페이지를 이용 안하겠다.
+// Spring Security에서 기본제공하는 로그인 페이지를 이용 안하겠다
 public class BoardProjectApplication {
 
 	public static void main(String[] args) {
