7494b6d00c76a24d827acb96c82791e111443880
huni1905-droid <huni1905@gmail.com>
2025-12-16 17:46:18 +0900

    12월 16일 수업

--- boardProject/src/main/java/edu/kh/project/common/config/FileConfig.java
+++ boardProject/src/main/java/edu/kh/project/common/config/FileConfig.java
@@ -1,87 +1,87 @@
 package edu.kh.project.common.config;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.unit.DataSize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import edu.kh.project.member.model.dto.Member;
 import jakarta.servlet.MultipartConfigElement;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@Configuration // 서바가 실행되자마자 Bean 으로 등록되서 아래 실행되는 설정용 클래스다
+@Configuration
 @PropertySource("classpath:/config.properties")
 public class FileConfig implements WebMvcConfigurer{
 	// WebMvcConfigurer : Spring MVC 프레임워크에서 제공하는 
-	// 인터페이스 중 하나로, 스프링 구성을 커스터마이징하고 
+	// 인터페이스 중 하나로, 스프링 구성을 커스터마이징하고
 	// 확장하기 위한 메서드를 제공함.
-	// 주로 웹 애플리케이이션의 설정을 조정하거나 추자하는데 사용됨
+	// 주로 웹 애플리케이션의 설정을 조정하거나 추가하는데 사용됨
 	
 	// 파일 업로드 임계값
 	@Value("${spring.servlet.multipart.file-size-threshold}")
 	private long fileSizeThreshold; // 52428800
 	
-	
 	// 임계값 초과시 파일의 임시 저장경로
 	@Value("${spring.servlet.multipart.location}")
 	private String location; // C:/uploadFiles/temp/
 	
-	
 	// 요청당 파일 최대 크기
 	@Value("${spring.servlet.multipart.max-request-size}")
-	private long maxRequestSize;  // 52428800
-	
+	private long maxRequestSize; // 52428800
 	
 	// 개별 파일당 최대 크기
 	@Value("${spring.servlet.multipart.max-file-size}")
 	private long maxFileSize; // 10485760
-
-	// --------------------------------------------------
+	
+	// -------------------------------------------
 	
 	// 프로필 이미지 관련 경로
 	@Value("${my.profile.resource-handler}")
-	private String profileResourceHandler;
-	//  /myPage/profile/**
+	private String profileResourceHandler; 
+	// /myPage/profile/**
 	
 	@Value("${my.profile.resource-location}")
-	private String profileResourceLocation;
-	//  file:///C:/uploadFiles/profile/
+	private String profileResourceLocation; 
+	// file:///C:/uploadFiles/profile/
+	
+	// -------------------------------------------
+	
+	// 게시판 이미지 관련 경로
+	@Value("${my.board.resource-handler}")
+	private String boardResourceHandler; // /images/board/**
+	
+	@Value("${my.board.resource-location}")
+	private String boardResourceLocation; // file:///C:/uploadFiles/board/
 	
 	
 	// 요청 주소에 따라
 	// 서버 컴퓨터의 어떤 경로에 접근할지 설정
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// ResourceHandlerRegistry
-		// Spring MVC에서 정적 리소스(이미지, CSS, JS 등)의 
+		// ResourceHandlerRegistry : 
+		// Spring MVC에서 정적 리소스(이미지, CSS, JS 등)의
 		// 요청을 처리하기 위해 사용하는 클래스
 		
 		// URL 요청 패턴을 서버의 실제 파일 경로와 연결하여
 		// 클라이언트가 특정 경로로 정적파일에 접근할 수 있도록 설정
-		registry.addResourceHandler("/myPage/file/**")
+		registry.addResourceHandler("/myPage/file/**") 
 		.addResourceLocations("file:///C:/uploadFiles/test/");
 		// -> 클라이언트가 /myPage/file/** 패턴으로 이미지를 요청할 때
 		// 서버 폴더 경로 중 C:/uploadFiles/test/ 로 연결 하겠다.
 		
-		
 		registry.addResourceHandler(profileResourceHandler)
 		.addResourceLocations(profileResourceLocation);
 		// -> 클라이언트가 /myPage/profile/** 패턴으로 이미지 요청할 때
-		// 서버 폴더 경로 중 C:/uploadFiles/profile/로 연결
+		// 서버 폴더 경로 중 C:/uploadFiles/profile/ 로 연결
+		
+		registry.addResourceHandler(boardResourceHandler)
+		.addResourceLocations(boardResourceLocation);
+		// -> 클라이언트가 /images/board/** 패턴으로 이미지 요청할 때
+		// 서버 폴더 경로중 C:/uploadFiles/board/ 로 연결
 		
 		
 	}
@@ -92,18 +92,18 @@ public class FileConfig implements WebMvcConfigurer{
 	// MultipartResolver 설정
 	@Bean
 	public MultipartConfigElement configElement() {
-		// MultipartConfigElement : 
+		// MultipartConfigElement :
 		// 파일 업로드를 처리하는데 사용되는 MultipartConfigElement를 
 		// 구성하고 반환(옵션 설정하는데 사용)
-		// 업로드 파일의 최대크기, 임시 저장 경로 등.. 
-		MultipartConfigFactory factory
+		// 업로드 파일의 최대크기, 임시 저장 경로 등..
+		MultipartConfigFactory factory 
 			= new MultipartConfigFactory();
 		
-		// 파일 업로드 임계값 
+		// 파일 업로드 임계값
 		factory.setFileSizeThreshold(DataSize.ofBytes(fileSizeThreshold));
 		
 		// 임시 저장 폴더 경로
-		factory.setLocation(location);		
+		factory.setLocation(location);
 		
 		// HTTP 요청당 파일 최대 크기
 		factory.setMaxRequestSize(DataSize.ofBytes(maxRequestSize));
@@ -114,7 +114,6 @@ public class FileConfig implements WebMvcConfigurer{
 		return factory.createMultipartConfig();
 	}
 	
-	
 	// MultipartResolver 객체를 생성하여 Bean으로 등록
 	// -> 위에서 만든 MultipartConfigElement 자동으로 이용함
 	@Bean
@@ -124,23 +123,10 @@ public class FileConfig implements WebMvcConfigurer{
 		// 그 중 업로드된 파일을 추출하여 MultipartFile 객체로 제공하는 역할
 		StandardServletMultipartResolver multipartResolver
 			= new StandardServletMultipartResolver();
-	
+		
 		return multipartResolver;
+		
 	}
 	
-	
-	@PostMapping("file/test3")	//  /myPage/file/test3 POST 요청 매핑
-	public String fileUpload3(@RequestParam("aaa") List<MultipartFile> aaaList,
-			@RequestParam("bbb") List<MultipartFile> bbbList,
-			@SessionAttribute("loginMember") Member loginMember,
-			RedirectAttributes ra) {
-		
-		log.debug("aaaList" + aaaList);		// [요소, 요소]
-		log.debug("bbbList" + bbbList);  	// [요소]
 
-		
-		return "redirect:/myPage/fileTest";
-	}
-	
-	
 }
