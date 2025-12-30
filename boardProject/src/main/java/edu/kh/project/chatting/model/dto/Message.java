9e387fd2d61f8f3c05b93a2ebfcf015b34a483d2
huni1905-droid <huni1905@gmail.com>
2025-12-30 21:57:36 +0900

    1230 수업

---
+++ boardProject/src/main/java/edu/kh/project/chatting/model/dto/Message.java
@@ -0,0 +1,22 @@
+package edu.kh.project.chatting.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+	private int messageNo;		  // 메시지 번호
+	private String messageContent;// 메시지 내용
+	private String readFl; 		  // 읽음 여부
+	private int senderNo; 		  // 보낸 회원
+	private int chattingRoomNo;   // 채팅방 번호
+	private String sendTime; 	  // 메시지 보낸 시간
+	private int targetNo;		  // 웹소켓을 이용한 메세지 값 세팅시 필요
+	
+	
+}
---
+++ boardProject/src/main/java/edu/kh/project/chatting/model/service/CharttingServiceImpl.java
@@ -0,0 +1,82 @@
+package edu.kh.project.chatting.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.project.chatting.model.dto.ChattingRoom;
+import edu.kh.project.chatting.model.dto.Message;
+import edu.kh.project.chatting.model.mapper.ChattingMapper;
+import edu.kh.project.member.model.dto.Member;
+
+@Service
+@Transactional
+public class CharttingServiceImpl implements ChattingService{
+	
+	@Autowired
+	private ChattingMapper mapper;
+	
+	// 채팅방 목록 조회
+	@Override
+	public List<ChattingRoom> selectRoomList(int memberNo) {
+		return mapper.selectRoomList(memberNo);
+	}
+	
+	
+	// 채팅 상대 검색 서비스
+	@Override
+	public List<Member> selectTarget(Map<String, Object> map) {
+		return mapper.selectTarget(map);
+	}
+	
+	// 기존 채팅방번호 체크 서비스
+	@Override
+	public int checkChattingRoomNo(Map<String, Integer> map) {
+		
+		return mapper.checkChattingRoomNo(map);
+	}
+	
+	// 새로운 채팅방 생성 서비스
+	@Override
+	public int createChattingRoom(Map<String, Integer> map) {
+		
+		int result = mapper.createChattingRoom(map);
+		
+		if(result >0 ) {
+			return map.get("chattingRoomNo");
+		}
+		
+		return 0;
+	}
+	
+	// 채팅 메시지 조회 서비스
+	@Override
+	public List<Message> selectMessageList(Map<String, Object> paramMap) {
+
+		List<Message> messageList = mapper.selectMessageList(paramMap.get("chattingRoomNo"));
+		
+		if(!messageList.isEmpty()) { // 해당 채팅방에서 나눈 메시지가 있다면
+			int result = mapper.updateReadFlag(paramMap);
+			
+		}
+		
+		return messageList;
+	}
+	
+	// 채팅 읽음 표시 업데이트 서비스
+	@Override
+	public int updateReadFlag(Map<String, Object> paramMap) {
+		
+		return mapper.updateReadFlag(paramMap);
+	}
+	
+	
+	@Override
+	public int insertMessage(Message msg) {
+		return mapper.insertMessage(msg);
+	}
+	
+}
